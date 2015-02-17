4c30f2d340ecd6489b5e09a135b7aec4c20c7533
Frederik Schmidt <frederikschmidt79@gmail.com>
2015-02-17 15:09:23 +1300

    .

--- rest-service/src/main/java/hello/Application.java
+++ rest-service/src/main/java/hello/Application.java
@@ -8,7 +8,16 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class Application 
 {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) 
+	 {
+		    String webPort = System.getenv("PORT");
+		    
+		    if (webPort == null || webPort.isEmpty()) 
+		    {
+		        webPort = "5000";
+		    }
+		    System.setProperty("server.port", webPort);
+		 
 	        SpringApplication.run(Application.class, args);
-	    }
+	  }
 }
